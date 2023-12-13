f274fc9340791256626c397a2845d0c3cea9a0dc
Mona Lisa <austinnguyenquoc@gmail.com>
2023-12-12 20:00:06 -0400

    modified eventscript

--- eventscript.js
+++ eventscript.js
@@ -1,6 +1,6 @@
 const apiKey2 = 'ZnIIvIGonV99xJ9qGt49z7OBV8nQqJSo'; // Replace with your Ticketmaster API key
 
-const searchEvents = async function(city) {
+const searchEvents = async () => {
     const cityInput = document.getElementById('cityInput').value;
     const resultsContainer = document.getElementById('results');
 
@@ -25,40 +25,44 @@ const displayEvents = (events, container) => {
     const ul = document.createElement('ul');
 
     events.forEach((event) => {
-        // Debugging: Log the event object to inspect its structure
-        console.log(event);
-
         const li = document.createElement('li');
-        const eventDate = event.dates.start.localDate; // Ensure this path is correct
-        
-        // Create an anchor tag for each event
-    const a = document.createElement('a');
-    a.href = event.url; // Replace with the actual property path for the event URL
-    a.textContent = `${event.name} - ${eventDate}`;
-    a.target = "_blank"; // Optional: Opens link in a new tab
 
-    // Append the anchor tag to the list item
-    li.appendChild(a);
-    ul.appendChild(li);
+        // Create an anchor tag for each event
+        const a = document.createElement('a');
+        a.href = event.url; // Use the correct property for the event URL
+        a.target = "_blank"; // Opens the link in a new tab
+
+        const eventName = document.createElement('strong');
+        eventName.textContent = event.name;
+
+        const eventDate = document.createElement('span');
+        const eventDateTime = new Date(event.dates.start.dateTime);
+        eventDate.textContent = ` - ${eventDateTime.toDateString()}`;
+
+        // Append the event name and date to the anchor tag
+        a.appendChild(eventName);
+        a.appendChild(eventDate);
+
+        // Append the anchor tag to the list item
+        li.appendChild(a);
+        ul.appendChild(li);
     });
 
-container.appendChild(ul);
+    container.appendChild(ul);
 };
-// Updated getEventsByCity function
 const getEventsByCity = async (city) => {
+    const apiKey2 = 'ZnIIvIGonV99xJ9qGt49z7OBV8nQqJSo'; // Replace with your Ticketmaster API key
     const baseURL = 'https://app.ticketmaster.com/discovery/v2/events.json';
 
-    // Calculate date range
+    // Calculate the start and end dates for the next 5 days
     const today = new Date();
-    const endDate = new Date();
-    endDate.setDate(today.getDate() + 5); // 5 days from today
+    const endDate = new Date(today);
+    endDate.setDate(today.getDate() + 5);
 
-    // Format dates in YYYY-MM-DD
-    const startDateStr = today.toISOString().split('T')[0];
-    const endDateStr = endDate.toISOString().split('T')[0];
+    const startDateString = today.toISOString().split('T')[0];
+    const endDateString = endDate.toISOString().split('T')[0];
 
-    // Include the date range in the query
-    const queryParams = `?apikey=${apiKey2}&city=${encodeURIComponent(city)}&startDateTime=${startDateStr}T00:00:00Z&endDateTime=${endDateStr}T23:59:59Z`;
+    const queryParams = `?apikey=${apiKey2}&city=${encodeURIComponent(city)}&startDateTime=${startDateString}T00:00:00Z&endDateTime=${endDateString}T23:59:59Z`;
 
     const url = `${baseURL}${queryParams}`;
 
@@ -69,10 +73,11 @@ const getEventsByCity = async (city) => {
         if (data._embedded && data._embedded.events) {
             return data._embedded.events;
         } else {
-            throw new Error('No events found for the specified city.');
+            throw new Error(`No events found for the city: ${city}`);
         }
     } catch (error) {
         console.error('Error fetching events:', error.message);
         throw error;
     }
 };
+
